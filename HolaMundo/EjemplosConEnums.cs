d0a4f3d0dbde986c37666ce3c0184df5a4335f8b
eoviedo <erwin.oviedo@gmail.com>
2017-09-03 23:30:59 -0400

    Instrucciones con If, Else, For, While, Constants and enums

---
+++ HolaMundo/EjemplosConEnums.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HolaMundo
+{
+    enum TipoDeDocumento {
+        CarnetIdentidad,
+        Pasaporte,
+        LibretaServicioMilitar
+    }
+    class EjemplosConEnums
+    {
+        static void Ejemplo()
+        {
+            TipoDeDocumento documento;
+
+            documento = TipoDeDocumento.CarnetIdentidad;
+
+            if (documento == TipoDeDocumento.CarnetIdentidad)
+            {
+                //Codigo
+            }
+            else if(documento == TipoDeDocumento.LibretaServicioMilitar)
+            {
+                //Codigo
+            }
+            else if(documento == TipoDeDocumento.Pasaporte)
+            {
+                //Codigo
+            }
+        }
+
+        static void UsandoSwitch()
+        {
+            TipoDeDocumento documento;
+
+            documento = TipoDeDocumento.CarnetIdentidad;
+
+            switch (documento)
+            {
+                case TipoDeDocumento.CarnetIdentidad:
+                    //Codigo
+                    break;
+                case TipoDeDocumento.Pasaporte:
+                    //Codigo
+                    break;
+                case TipoDeDocumento.LibretaServicioMilitar:
+                    //Codigo
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+}
--- HolaMundo/Program.cs
+++ HolaMundo/Program.cs
@@ -10,6 +10,79 @@ namespace HolaMundo
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Hola Mundo");
+            InicializandoVariables();
+            UsandoIfElse();
+            UsandoFor();
+            UsandoWhile();
+            UsandoDoWhile();
+            Console.Read();
         }
+        /// <summary>
+        /// El proposito del siguiente codigo es demostrar diferentes formas que existen para inicializar una variable
+        /// </summary>
+        static void InicializandoVariables()
+        {
+            string nombre = "Daniel";
+            var apellido = "Perez";
+            var fechaNacimiento = new DateTime(1997, 8, 30);
+            var edad = 20;
+
+            Console.WriteLine($"{nombre} {apellido} tiene {edad} años y su fecha de nacimiento es {fechaNacimiento.ToShortDateString()}");
+        }
+
+        static void UsandoIfElse()
+        {
+            var Maria = 20;
+
+            if(Maria > 18)
+            {
+                Console.WriteLine("Maria es mayor de edad");
+            }
+            else if (Maria > 11 && Maria <18)
+            {
+                Console.WriteLine("Maria es adolescente");
+            }
+            else
+            {
+                Console.WriteLine("Maria es niña");
+            }
+        }
+
+        static void UsandoFor()
+        {
+            for (int i = 0; i < 300; i++)
+            {
+                Console.WriteLine($"Hola {i}");
+            }
+        }
+
+        static void UsandoWhile()
+        {
+            var totalPersonas = 0;
+            while (totalPersonas < 20)
+            {
+                Console.WriteLine($"Total personas = {totalPersonas}");
+                totalPersonas++;
+            }
+        }
+        static void UsandoDoWhile()
+        {
+            var totalPersonas = 0;
+            do
+            {
+                Console.WriteLine($"Total personas = {totalPersonas}");
+                totalPersonas++;
+            }
+            while (totalPersonas < 20);
+        }
+        static void UsandoConstants()
+        {
+            const double piValue = 3.141592653589;
+            const int mayoriaDeEdad = 18;            
+
+        }
+
+        
     }
 }
